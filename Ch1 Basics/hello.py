c6f3fa2066c505dc036ed1a3c617e3d73c1b7713
MyStuffGoesHere <hentysenpai@gmail.com>
2025-04-02 13:07:55 -0700

    Completed first Program

---
+++ Ch1 Basics/hello.py
@@ -0,0 +1,13 @@
+# This Program Says Hello And Asks for your name.
+
+print('Hello!')
+print('What is your name?') # Asks for name
+
+myName = input() # Takes input from user to get Name
+print('It is good to meet you!, ' + myName)
+print('The length of your name is:')
+print(len(myName)) # Tells the length of the name given
+
+print('what is your age?')
+myAge = input()
+print('you will be'+ str(int(myAge) + 2) + 'in 2 years!')
